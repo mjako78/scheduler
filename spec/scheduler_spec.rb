1dd2b33afae58e0f7d233d94b2b6c18c6e489ea1
Mauro Giacomini <mauro.giaco@gmail.com>
2017-07-13 21:54:01 +0200

    Create gem structure

---
+++ spec/scheduler_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Scheduler do
+  it "has a version number" do
+    expect(Scheduler::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
